4d0b64882a2d6ec1345e5a23a5e7d7818f8b82fe
s153430 <s153430@student.dtu.dk>
2019-11-11 14:56:11 +0100

    Changes to newloader so it equally distributes the images

--- scripts/newLoader.py
+++ scripts/newLoader.py
@@ -35,12 +35,22 @@ class Fishy(torch.utils.data.Dataset):
                 data_path : Path to images
         """
         self.transform = transform
-        data_path = os.path.join(data_path, 'train' if train else 'test')
+        #data_path = os.path.join(data_path, 'train' if train else 'test')
         fp = open(category_path, 'r')
         #i.split(";")[1][1:-1]
 
-        self.fish_dict =  {i.split(";")[0] : fishies[i.split(";")[1][1:-1]] for i in fp}
+        #self.fish_dict =  {i.split(";")[0] : fishies[i.split(";")[1][1:-1]] for i in fp}
+        self.fish_dict = {}
+        count_categories = 0
+        for i in fp:
+            if self.fish_dict.get(fishies[i.split(";")[1][1:-1]]):
+                self.fish_dict[fishies[i.split(";")[1][1:-1]]].append(i.split(";")[0])
+            else:
+                self.fish_dict[fishies[i.split(";")[1][1:-1]]] = [i.split(";")[0]]
+                count_categories+=1
         
+        self.mostPicturesSameCat = max([len(v) for k,v in self.fish_dict.items()])
+        self.lengthOfArray = self.mostPicturesSameCat * count_categories
         #self.name_to_label = [i.split(";")[1][1:-1] for i in fp]
         self.image_paths = glob.glob(data_path + '/*.jpg')
         
@@ -50,7 +60,7 @@ class Fishy(torch.utils.data.Dataset):
         Returns :
             int : The total number of images
         """
-        return len(self.image_paths)
+        return self.lengthOfArray
 
     def __getitem__(self, idx):
         """
@@ -62,28 +72,39 @@ class Fishy(torch.utils.data.Dataset):
         Returns :
             Image : Transformed image.
         """
-        image_path = self.image_paths[idx]
+
+        category = idx//self.mostPicturesSameCat
+
+        pictures = self.fish_dict[category]
+        idLookup = idx - self.mostPicturesSameCat * category
+        picture = pictures[idLookup % len(pictures)]
+
+        image_path = "../../../extracted/" + picture + ".jpg" #self.image_paths[idx] 
         
-        lookup = image_path.split("/")[-1].split(".")[0]
+        #lookup = image_path.split("/")[-1].split(".")[0]
         
         image = Image.open(image_path)
         #y = self.name_to_label[idx]
-        y = self.fish_dict[lookup]
+        #y = self.fish_dict[lookup]
         X = self.transform(image)
-        return X,y
+        return X,category
 
-
-def createDataLoaders(batch_size, size):
+def createDataLoaders(batch_size, size,rotation = 45, train_distribution = 0.8):
     #For testing
     transform = transforms.Compose(
         [transforms.Resize((size,size)),
+        transforms.RandomRotation(rotation),
+        transforms.RandomVerticalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5),
                             (0.5, 0.5, 0.5))]
     )
-
-    trainset = Fishy(train=True, transform=transform)
-    testset = Fishy(train=False, transform=transform)
+    full_dataset = Fishy(train=True, transform=transform)
+    train_size = int(train_distribution * len(full_dataset))
+    test_size = len(full_dataset) - train_size
+    trainset, testset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
+    #trainset = Fishy(train=True, transform=transform)
+    #testset = Fishy(train=False, transform=transform)
     train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=6)
     test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=6)
 

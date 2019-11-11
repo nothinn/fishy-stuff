e701f7e38965ae5c07d7f755697283a3908b4b9e
s153430 <s153430@student.dtu.dk>
2019-11-11 15:22:28 +0100

    Try/catch when keyinterrupt in main.py

--- scripts/Main.py
+++ scripts/Main.py
@@ -39,49 +39,53 @@ batch_size = 64
 trainingloader, testloader, trainset, testset = newLoader.createDataLoaders(batch_size, image_size)
 train_acc_all = []
 test_acc_all = []
-for epoch in tqdm(range(num_epoch)):
-    running_loss = 0.0
-    train_correct = 0
-    for i, data in enumerate(trainingloader, 0):
+try:
+    for epoch in tqdm(range(num_epoch)):
+        running_loss = 0.0
+        train_correct = 0
+        for i, data in enumerate(trainingloader, 0):
 
-        inputs, labels = data
-        inputs, labels = Variable(inputs).to(device), Variable(labels).to(device)
+            inputs, labels = data
+            inputs, labels = Variable(inputs).to(device), Variable(labels).to(device)
 
-        optimizer.zero_grad()
+            optimizer.zero_grad()
 
-        optimizer
+            optimizer
 
-        outputs = network(inputs)
+            outputs = network(inputs)
 
-        loss = criterion(outputs, labels)
-        loss.backward()
+            loss = criterion(outputs, labels)
+            loss.backward()
 
-        optimizer.step()
+            optimizer.step()
 
-        running_loss += loss
+            running_loss += loss
 
-        predicted = outputs.argmax(1)
-        train_correct += (labels==predicted).sum().cpu().item()
+            predicted = outputs.argmax(1)
+            train_correct += (labels==predicted).sum().cpu().item()
+            
+            if i % 100 == 1:
+                print("[%d, %5d] loss: %.3f" % 
+                    (epoch + 1, i + 1, running_loss/1000))
+                running_loss = 0.0
         
-        if i % 100 == 1:
-            print("[%d, %5d] loss: %.3f" % 
-                (epoch + 1, i + 1, running_loss/1000))
-            running_loss = 0.0
+        network.eval()
+        test_correct = 0.0
+        for data, target in testloader:
+            data = data.to(device)
+            with torch.no_grad():
+                output = network(data)
+            predicted = output.argmax(1).cpu()
+            test_correct += (target==predicted).sum().item()
+        train_acc = train_correct/len(trainset)
+        test_acc = test_correct/len(testset)
+        train_acc_all.append(train_acc)
+        test_acc_all.append(test_acc)
+        print("Accuracy train: {train:.1f}%\t test: {test:.1f}%".format(test=100*test_acc, train=100*train_acc))
+except(KeyboardInterrupt):
+    print("Model saved")
+    torch.save(network, "model.pt")
     
-    network.eval()
-    test_correct = 0.0
-    for data, target in testloader:
-        data = data.to(device)
-        with torch.no_grad():
-            output = network(data)
-        predicted = output.argmax(1).cpu()
-        test_correct += (target==predicted).sum().item()
-    train_acc = train_correct/len(trainset)
-    test_acc = test_correct/len(testset)
-    train_acc_all.append(train_acc)
-    test_acc_all.append(test_acc)
-    print("Accuracy train: {train:.1f}%\t test: {test:.1f}%".format(test=100*test_acc, train=100*train_acc))
-
 print("training over")
 
 torch.save(network, "model.pt")

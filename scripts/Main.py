15327a0368835aa14742213097614e83fa5a9742
s153430 <s153430@student.dtu.dk>
2019-11-18 14:41:50 +0100

    Added tensorboard

--- scripts/Main.py
+++ scripts/Main.py
@@ -5,6 +5,8 @@ import torch.optim as optim
 from torch.autograd import Variable
 import numpy as np
 import torch
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
 
 from tqdm import tqdm
 
@@ -21,6 +23,9 @@ conv_stride     = 1
 conv_pad        = 1
 conv_drop_rate  = 0.4
 image_size      = 64
+
+# Writer will output to ./runs/ directory by default
+writer = SummaryWriter()
 
 network = Network.Net(channels, kernel_size,conv_stride, conv_pad, conv_drop_rate, num_classes, image_size)
 print(network)
@@ -64,7 +69,7 @@ try:
             predicted = outputs.argmax(1)
             train_correct += (labels==predicted).sum().cpu().item()
             
-            if i % 100 == 1:
+            if i % 10 == 1:
                 print("[%d, %5d] loss: %.3f" % 
                     (epoch + 1, i + 1, running_loss/1000))
                 running_loss = 0.0
@@ -77,14 +82,22 @@ try:
                 output = network(data)
             predicted = output.argmax(1).cpu()
             test_correct += (target==predicted).sum().item()
+
         train_acc = train_correct/len(trainset)
         test_acc = test_correct/len(testset)
         train_acc_all.append(train_acc)
         test_acc_all.append(test_acc)
+        #writer.add_scalar('Loss/train', train_acc, epoch)
+        #writer.add_scalar('Loss/test', np.random.random(), epoch)
+        writer.add_scalar('Accuracy/train', train_correct*100//len(trainset), epoch + 1)
+        writer.add_scalar('Accuracy/test', test_correct*100//len(testset), epoch + 1)
+        if epoch % 3 == 0:
+            writer.flush()
         print("Accuracy train: {train:.1f}%\t test: {test:.1f}%".format(test=100*test_acc, train=100*train_acc))
 except(KeyboardInterrupt):
     print("Model saved")
     torch.save(network, "model.pt")
+    writer.close()
     
 print("training over")
 
